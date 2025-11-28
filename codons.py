d1171ad6b7062fd7e39ba95f3b437786436307d2
moriah-schwartz <moriahschwartz12@gmail.com>
2025-11-28 15:04:57 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,10 +1,14 @@
 def create_codon_dict(file_path):
-    codon_to_amino = {}
-    for row in rows:
+  path = "data/codons.txt"
+  file = open(path)
+  rows = file.readlines()
+  file.close()
+  codon_to_amino = {}
+  for row in rows:
      cells = row.strip().split('\t')
      codon = cells[0]
      amino = cells[2]
      codon_to_amino[codon] = amino
-    return codon_to_amino
+  return codon_to_amino
 
 
